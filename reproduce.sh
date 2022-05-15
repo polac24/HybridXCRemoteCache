da2de87a162f83141733a99d593c7e05f398f98c
Bartosz Polaczyk <polac24@gmail.com>
2022-05-15 19:08:02 +0200

    Add reproduce steps

---
+++ reproduce.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+git remote add self .
+git fetch self
+
+rm -rf DD
+git checkout -f
+rm -rf ~/Library/Caches/XCRemoteCache/localhost
+
+# start ngingx
+sudo nginx -s stop
+sudo nginx -c $PWD/nginx.conf 
+
+# reset stats
+XCRC/xcprepare stats --reset
+
+## Producer
+XCRC/xcprepare integrate --input Hybrid.xcodeproj --mode producer --final-producer-target Hybrid
+xcodebuild EXCLUDED_ARCHS=x86_64  -project Hybrid.xcodeproj -destination 'generic/platform=iOS Simulator' -scheme Hybrid  -sdk "iphonesimulator" -derivedDataPath DD > build.log
+
+## print & reset stats
+XCRC/xcprepare stats
+XCRC/xcprepare stats --reset
+
+## Consumer
+XCRC/xcprepare integrate --input Hybrid.xcodeproj --mode consumer --final-producer-target Hybrid
+xcodebuild EXCLUDED_ARCHS=x86_64 -project Hybrid.xcodeproj -destination 'generic/platform=iOS Simulator' -scheme Hybrid -sdk "iphonesimulator" -derivedDataPath  DD > build.log
+
+## print stats
+XCRC/xcprepare stats
+XCRC/xcprepare stats --reset
+
+# Modify the Swift file to force to local compilation
+sed -i '' 's/print/ print/' Hybrid/ViewController.swift 
+# Build and see if any ObjC step is invoked from Xcode
+xcodebuild -project Hybrid.xcodeproj -destination 'generic/platform=iOS Simulator' -scheme Hybrid -sdk "iphonesimulator" -derivedDataPath DD | tee build.log | grep CompileC
+
+XCRC/xcprepare stats
+
+## stop nginx (temporarly disabled)
+# sudo nginx -s stop
