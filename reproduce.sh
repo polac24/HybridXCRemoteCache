e34d6c0d63bcaf31122aff6389b5eeaedccdc278
Bartosz Polaczyk <polac24@gmail.com>
2022-05-15 19:32:58 +0200

    Sleep

--- reproduce.sh
+++ reproduce.sh
@@ -16,7 +16,7 @@ XCRC/xcprepare stats --reset
 
 ## Producer
 XCRC/xcprepare integrate --input Hybrid.xcodeproj --mode producer --final-producer-target Hybrid
-xcodebuild EXCLUDED_ARCHS=x86_64  -project Hybrid.xcodeproj -destination 'generic/platform=iOS Simulator' -scheme Hybrid  -sdk "iphonesimulator" -derivedDataPath DD > build.log
+xcodebuild EXCLUDED_ARCHS=x86_64  -project Hybrid.xcodeproj -destination 'platform=iOS Simulator,name=iPhone 12' -scheme Hybrid  -sdk "iphonesimulator" -derivedDataPath DD > build.log
 
 ## print & reset stats
 XCRC/xcprepare stats
@@ -24,7 +24,7 @@ XCRC/xcprepare stats --reset
 
 ## Consumer
 XCRC/xcprepare integrate --input Hybrid.xcodeproj --mode consumer --final-producer-target Hybrid
-xcodebuild EXCLUDED_ARCHS=x86_64 -project Hybrid.xcodeproj -destination 'generic/platform=iOS Simulator' -scheme Hybrid -sdk "iphonesimulator" -derivedDataPath  DD > build.log
+xcodebuild EXCLUDED_ARCHS=x86_64 -project Hybrid.xcodeproj -destination 'platform=iOS Simulator,name=iPhone 12' -scheme Hybrid -sdk "iphonesimulator" -derivedDataPath  DD > build.log
 
 ## print stats
 XCRC/xcprepare stats
@@ -33,7 +33,10 @@ XCRC/xcprepare stats --reset
 # Modify the Swift file to force to local compilation
 sed -i '' 's/print/ print/' Hybrid/ViewController.swift 
 # Modify history.compile to fail a build if that history is actually relevant (invoked)
-LC_CTYPE=C LANG=C sed -i '' 's/clang/nonexisting/' DD/Build/Intermediates.noindex/Hybrid.build/Debug-iphonesimulator/Hybrid.build/history.compile 
+#LC_CTYPE=C LANG=C sed -i '' 's/clang/nonexisting/' DD/Build/Intermediates.noindex/Hybrid.build/Debug-iphonesimulator/Hybrid.build/history.compile 
+printf "/bin/sleep\0\"100\"\0\0\n" >> DD/Build/Intermediates.noindex/Hybrid.build/Debug-iphonesimulator/Hybrid.build/history.compile 
+cp DD/Build/Intermediates.noindex/Hybrid.build/Debug-iphonesimulator/Hybrid.build/history.compile .
+
 # Build and see if any ObjC step is invoked from Xcode
 xcodebuild -project Hybrid.xcodeproj -destination 'generic/platform=iOS Simulator' -scheme Hybrid -sdk "iphonesimulator" -derivedDataPath DD | tee build.log | grep CompileC
 
